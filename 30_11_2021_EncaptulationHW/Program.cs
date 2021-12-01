5b8447e4ffd66cff8adc3e4c60a3417ba11e904b
HakanDARAK <hakandarak1@gmail.com>
2021-12-01 11:50:48 +0300

    Proje dosyası ekle.

---
+++ 30_11_2021_EncaptulationHW/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_11_2021_EncaptulationHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string karar = string.Empty;
+            string karar1 = string.Empty;
+            string karar3 = string.Empty;
+            personel personel = new personel();
+            do
+            {
+                Console.WriteLine("Lütfen TC kimlik numaranızı Giriniz");
+                string tcNo = Console.ReadLine();
+                personel.TcNo = tcNo;
+                Console.WriteLine("En son başarılı olarak girdiginiz TC kimlik numarasının ilk beş hanesini görmek istiyor musunuz? Evetse E Hayırsa H yazınız.");
+                karar1 = Console.ReadLine();
+                string karar2= karar1.ToUpper();
+                if (karar2 == "E")
+                {
+                    Console.WriteLine(personel.TcNo);
+                }
+                Console.WriteLine("Ekleme yapmak istiyor musunuz? Evetse E Hayırsa H yazınız.");
+                karar = Console.ReadLine();
+               karar3= karar.ToUpper();
+            } while (karar3 != "H");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 30_11_2021_EncaptulationHW/personel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_11_2021_EncaptulationHW
+{
+    public class personel
+    {
+        private string _TcNo;
+        public string TcNo
+        {
+            get
+            {
+                return _TcNo.ToString().Substring(0, 5);
+            }
+            set
+            {
+                try
+                {
+                    int sayac = 0;
+                    bool control = false;
+                    if (value.ToString().Length == 11)
+                    {
+                        for (int i = 0; i < value.Length; i++)
+                        {
+                            control = char.IsNumber(value[i]);
+                            if (control)
+                            {
+                                sayac++;
+                            }
+
+                        }
+                        if (sayac == 11)
+                        {
+                            _TcNo = value;
+                            Console.WriteLine("Başarılı bir şekilde kayıt oluşturuldu");
+                        }
+                        else
+                        {
+                            Console.WriteLine("HATA! TC kimlik no sadece rakamlardan oluşturulmalıdır");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("HATA! TC kimlik numarası 11 haneli olmalıdır");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("HATA! : " + ex);
+                }
+            }
+        }
+    }
+}
